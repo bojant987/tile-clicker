e0481ffdcab6858623c1ee1d0619087caa596c9d
Bojan Todorovic <c.btodorovic@gogoair.com>
2018-07-30 15:12:03 +0200

    Cleared opened chart on player change

--- src/Redux/reducers/openedChart.js
+++ src/Redux/reducers/openedChart.js
@@ -6,6 +6,8 @@ export default function openedChart(state = initialState, action) {
 	switch (action.type) {
 		case actionTypes.OPEN_CHART:
 			return action.scoreId;
+		case actionTypes.CHOOSE_PLAYER:
+			return initialState;
 		default:
 			return state;
 	}
